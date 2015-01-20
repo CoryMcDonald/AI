28942278ce7e8730ffb01447c7294e196e2f4ae6
Cory McDonald <cory@corywmcdonald.com>
2015-01-19 21:40:39 -0600

    Finish homework

--- Assignment1/AI.py
+++ Assignment1/AI.py
@@ -1,10 +1,11 @@
 from PIL import Image
 from Queue import PriorityQueue
 import time
+import copy
+import heapq
 
 ImageObject = None
-distance = {}
-
+used = []
 
 class MyState:
     cost = None
@@ -24,39 +25,22 @@ class MyState:
         else:
             return False
 
-    def has_already_been_reached(self):
-        checkParent = self.parent
-        while checkParent is not None:
-            if checkParent.x == self.x and checkParent.y == self.y or self.x < 0 or self.y < 0 or self.x >= 500 or self.y >= 500:
-                return True
-            else:
-                checkParent = checkParent.parent
-        return False
 
-
-def transition_cost(s, c):
-    return getGreenColor(c.x, c.y)
-
-def exists(q, item):
-    while not q.empty():
-        prio, s = q.get()
-        if(s == item): 
-            return True
-    return False
 def breadth_first_search(startState, goalState):
-    q = PriorityQueue(0)
-    q.put((startState.cost, startState))
+    q = []
+    heapq.heappush(q, (startState.cost, startState))
     i = 0
-    while not q.empty():
+    while len(q) > 0:
         i = i + 1
-        prio, s = q.get()
+        s = heapq.heappop(q)[1]
         if(i % 5000 < 1000):
             setPixelGreen(s.x, s.y)
 
-        if s.parent and s.parent.x and s.parent.y:
-            print('currently on ' +  str(s.x) + ',' + str(s.y) + '. parent is ' + str(s.parent.x) + ',' + str(s.parent.y) + ' cost is ' + str(s.cost))
-
         if s.isEqual(goalState):
+            parentState = s
+            while parentState.parent is not None:
+                setPixelRed(parentState.x, parentState.y)
+                parentState = parentState.parent
             return s
 
         up = MyState(None, s, s.x, s.y - 1)
@@ -66,13 +50,12 @@ def breadth_first_search(startState, goalState):
 
         borders = [up, down, left, right]
         for c in borders:
-            if not c.has_already_been_reached():
+            if not used[c.x][c.y] and not (c.x < 0 or c.x >= 500) and not (c.y < 0 or c.y >= 500):
+                used[c.x][c.y] = True
                 c.parent = s
-                c.cost = s.cost + transition_cost(s, c)
-                q.put((c.cost, c))
-            else:
-                print (str(c.x) + ',' + str(c.y) + ' - has been used')
-        # time.sleep(1)
+                c.cost = s.cost + getGreenColor(c.x, c.y)
+                heapq.heappush(q, (c.cost, c))
+
     raise Exception("There is no path to the goal")
 
 
@@ -80,24 +63,18 @@ def getGreenColor(x, y):
     return ImageObject.getpixel((x, y))[2]
 
 
+def setPixelRed(x, y):
+    ImageObject.putpixel((x, y), (255, 0, 0, 255))
+
+
 def setPixelGreen(x, y):
-    currentPixel = list(ImageObject.getpixel((x, y)))
-    currentPixel[0] = 0
-    currentPixel[1] = 255
-    currentPixel[2] = 0
-    ImageObject.putpixel((x, y), tuple(currentPixel))
-    # ImageObject.close();
-    # 0=alpha, 1=blue, 2=green, 3=red
+    ImageObject.putpixel((x, y), (0, 255, 0, 255))
 
 
 ImageObject = Image.open("terrain.png")
+used = [[False for i in range(ImageObject.size[0] + 1)]
+        for j in range(ImageObject.size[1] + 1)]
 origin = MyState(0.0, None, 100, 100)
-goal = MyState(0.0, None, 102, 102)
+goal = MyState(0.0, None, 400, 400)
 print breadth_first_search(origin, goal).cost
 ImageObject.save('path.png', None)
-
-
-# for x in range(0, 300):
-#     setPixelGreen(200, x)
-#     for y in range(x, 200):
-#       setPixelGreen(x,y)
