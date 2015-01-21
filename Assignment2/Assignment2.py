0aa999527f8f84a659339be6cbfc265d173c61dc
Cory McDonald <cory@corywmcdonald.com>
2015-01-21 12:04:54 -0600

    Adding initial homework 2

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,11 +1,8 @@
-from PIL import Image
 from Queue import PriorityQueue
 import time
 import copy
 import heapq
 
-ImageObject = None
-used = []
 
 class MyState:
     cost = None
@@ -26,11 +23,10 @@ class MyState:
             return False
 
 
+
 def breadth_first_search(startState, goalState):
     q = []
-    beenThere = {}
     heapq.heappush(q, (startState.cost, startState))
-    beenThere.add(startState)
     i = 0
     while len(q) > 0:
         i = i + 1
@@ -52,40 +48,40 @@ def breadth_first_search(startState, goalState):
 
         borders = [up, down, left, right]
         for c in borders:
-            if(c in beenthere):
-                if(s.cost + cost < c.cost):
-                    c.cost = s.cost + acost;
-                    c.parent = s;
-            else:
+            if not used[c.x][c.y] and not (c.x <= 0 or c.x >= ImageObject.size[0]) and not (c.y <= 0 or c.y >= ImageObject.size[1]):
+                used[c.x][c.y] = True
                 c.parent = s
                 c.cost = s.cost + getGreenColor(c.x, c.y)
                 heapq.heappush(q, (c.cost, c))
-                beenThere.add(c)
-
-            # if not used[c.x][c.y] and not (c.x <= 0 or c.x >= ImageObject.size[0]) and not (c.y <= 0 or c.y >= ImageObject.size[1]):
 
     raise Exception("There is no path to the goal")
 
+class MyState:
+    cost = None
+    parent = None
+    x = None
+    y = None
 
-def getGreenColor(x, y):
-    return (255 -ImageObject.getpixel((x, y))[2])
+    def __init__(self, cost, par, x, y):
+        self.cost = cost
+        self.parent = par
+        self.x = x
+        self.y = y
 
+    def isEqual(self, state):
+        if self.x == state.x and self.y == state.y:
+            return True
+        else:
+            return False
 
-def setPixelRed(x, y):
-    ImageObject.putpixel((x, y), (255, 0, 0, 255))
+puzzle = 	'##########' + \
+		    '###  aa###' + \
+		    '##   a9 ##' + \
+		    '#00 #99  #' + \
+		    '#00##67  #' + \
+		    '#12266778#' + \
+		    '#112 6788#' + \
+		    '## 53344##' + \
+		    '###5534###' + \
+		    '##########'
 
-
-def setPixelGreen(x, y):
-    ImageObject.putpixel((x, y), (0, 255, 0, 255))
-
-# Maze2 - 120,241 ; 120, 0
-# Maze3 580, 1115 580
-
-ImageObject = Image.open("maze3.png")
-used = [[False for i in range(ImageObject.size[0] + 1)]
-        for j in range(ImageObject.size[1] + 1 )]
-origin = MyState(0.0, None, 1, 580)
-goal = MyState(0.0, None, 1115, 580)
-breadth_first_search(origin, goal)
-# ImageObject.resize((ImageObject.size[0]/2, ImageObject.size[1]/2)).save('maze3_solved.png', None)
-ImageObject.save('maze3_solved.png', None)
