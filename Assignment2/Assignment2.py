edd3a87de45c8556c958adb38ca69ce5b9e4f2e4
Cory McDonald <cory@corywmcdonald.com>
2015-01-27 20:15:02 -0600

    Working on homework 2

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,7 +1,12 @@
+from PIL import Image
 from Queue import PriorityQueue
 import time
+import math
 import copy
 import heapq
+
+ImageObject = None
+used = []
 
 
 class MyState:
@@ -23,23 +28,30 @@ class MyState:
             return False
 
 
-
 def breadth_first_search(startState, goalState):
     q = []
+    explored = {}
     heapq.heappush(q, (startState.cost, startState))
     i = 0
     while len(q) > 0:
         i = i + 1
         s = heapq.heappop(q)[1]
         # if(i % 5000 < 1000):
-        #     setPixelGreen(s.x, s.y)
+            # setPixelGreen(s.x, s.y)
 
         if s.isEqual(goalState):
             parentState = s
             while parentState.parent is not None:
                 setPixelRed(parentState.x, parentState.y)
                 parentState = parentState.parent
+            # while len(q) > 0:
+                # s = heapq.heappop(q)[1]
+            print 'Iterations',i
             return s
+
+        setPixelGreen(s.x, s.y) 
+        if i % 5000 == 0 or i==1:
+            ImageObject.save('path.png', None)
 
         up = MyState(None, s, s.x, s.y - 1)
         down = MyState(None, s, s.x, s.y + 1)
@@ -47,41 +59,48 @@ def breadth_first_search(startState, goalState):
         right = MyState(None, s, s.x + 1, s.y)
 
         borders = [up, down, left, right]
-        for c in borders:
-            if not used[c.x][c.y] and not (c.x <= 0 or c.x >= ImageObject.size[0]) and not (c.y <= 0 or c.y >= ImageObject.size[1]):
-                used[c.x][c.y] = True
-                c.parent = s
-                c.cost = s.cost + getGreenColor(c.x, c.y)
-                heapq.heappush(q, (c.cost, c))
 
+        for c in borders:
+            if not (c.x < 0 or c.x >= 500) and not (c.y < 0 or c.y >= 500):
+                # new_cost = cost_so_far[current] + graph.cost(current, next)
+                new_cost = s.cost + getGreenColor(c.x, c.y) 
+                if (c.x, c.y) not in explored or ((c.x,c.y) in explored and new_cost < explored[(c.x,c.y)].cost):
+                    c.parent = s
+                    # c.cost = s.cost + getGreenColor(c.x, c.y) 
+                    c.cost = new_cost + h(c.x, c.y)
+                    heapq.heappush(q, (c.cost, c))
+
+                    explored[(c.x, c.y)] = c;
+                # else:
+                    # if new_cost < explored[(c.x,c.y)].cost:
+                    #     print 'BOOYA', new_cost, explored[(c.x,c.y)].cost
+                    # else:
+                    #     print new_cost, explored[(c.x,c.y)].cost
+                    # time.sleep(.1)
+        # time.sleep(5)
     raise Exception("There is no path to the goal")
 
-class MyState:
-    cost = None
-    parent = None
-    x = None
-    y = None
 
-    def __init__(self, cost, par, x, y):
-        self.cost = cost
-        self.parent = par
-        self.x = x
-        self.y = y
+def getGreenColor(x, y):
+    return ImageObject.getpixel((x, y))[2]
 
-    def isEqual(self, state):
-        if self.x == state.x and self.y == state.y:
-            return True
-        else:
-            return False
+def setPixelRed(x, y):
+    ImageObject.putpixel((x, y), (255, 0, 0, 255))
 
-puzzle = 	'##########' + \
-		    '###  aa###' + \
-		    '##   a9 ##' + \
-		    '#00 #99  #' + \
-		    '#00##67  #' + \
-		    '#12266778#' + \
-		    '#112 6788#' + \
-		    '## 53344##' + \
-		    '###5534###' + \
-		    '##########'
+def h(x,y):
+    return abs(x-400) + abs(y-400)
 
+def setPixelGreen(x, y):
+    ImageObject.putpixel((x, y), (0, 255, 0, 255))
+
+# Maze2 - 120,241 ; 120, 0
+# Maze3 580, 1115 580
+
+ImageObject = Image.open("terrain.png")
+used = [[False for i in range(ImageObject.size[0] + 1)]
+        for j in range(ImageObject.size[1] + 1)]
+origin = MyState(0.0, None, 100, 100)
+goal = MyState(0.0, None, 400, 400)
+print breadth_first_search(origin, goal).cost
+# ImageObject.resize((ImageObject.size[0]/2, ImageObject.size[1]/2)).save('maze3_solved.png', None)
+ImageObject.save('path.png', None)
